5ca056fc7f97539d508d81d3cc4ed67d2dd3ec86
Julian Gentges <JulGentges@gmail.com>
2019-11-15 21:39:51 +0100

    add missing generic types

--- Sources/HereProvider/Models/Routing/ResponseDataTypes/HereRoutingDataTypes.swift
+++ Sources/HereProvider/Models/Routing/ResponseDataTypes/HereRoutingDataTypes.swift
@@ -318,7 +318,7 @@ public struct RouteNoteType: Content {
     public let type: RouteNoteTypeType
     public let code: RouteNoteCodeType
     public let text: String
-    public let additionalData: [KeyValuePairType]
+    public let additionalData: [KeyValuePairType<String,String>]
     public let countryChangeDetails: CountryChangeDetailsType?
     public let zoneIds: [UInt]
 }
