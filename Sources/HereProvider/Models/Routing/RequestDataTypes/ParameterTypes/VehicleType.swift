281899628c27a3966ec1233482ca2d9692235c19
Julian Gentges <JulGentges@gmail.com>
2019-11-17 22:30:55 +0100

    add encoding and decoding for 'LicensePlate'

--- Sources/HereProvider/Models/Routing/RequestDataTypes/ParameterTypes/VehicleType.swift
+++ Sources/HereProvider/Models/Routing/RequestDataTypes/ParameterTypes/VehicleType.swift
@@ -17,8 +17,31 @@ public enum TruckTypeType: String, Content {
     case truck, tractorTruck
 }
 
-public enum LicensePlate: Content {
+public enum LicensePlate{
     case lastCharacter(Int)
+}
+
+extension LicensePlate: Content {
+    enum CodingKeys: String, CodingKey {
+        case lastCharacter
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        if let lastCharacterValue = try container.decodeIfPresent(type: Int.self, forKey: .lastCharacter) {
+            self = .lastCharacter(lastCharacterValue)
+        }
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = try encoder.container(keyedBy: CodingKeys.self)
+        
+        switch self {
+        case .lastCharacter(let value):
+            try container.encode(value, forKey: .lastCharacter)
+        }
+    }
 }
 
 public enum TunnelCategory: String, Content {
