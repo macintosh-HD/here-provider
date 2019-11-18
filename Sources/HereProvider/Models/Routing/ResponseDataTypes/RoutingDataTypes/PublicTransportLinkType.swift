98f9ebf4f02cb8c3f5ff23f63933e13d99765c7e
Julian Gentges <JulGentges@gmail.com>
2019-11-18 14:15:02 +0100

    remove 'override' from initialiser

--- Sources/HereProvider/Models/Routing/ResponseDataTypes/RoutingDataTypes/PublicTransportLinkType.swift
+++ Sources/HereProvider/Models/Routing/ResponseDataTypes/RoutingDataTypes/PublicTransportLinkType.swift
@@ -4,7 +4,7 @@ public final class PublicTransportLinkType: RouteLinkType {
     public let nextStopName: String?
     public let line: ElementReferenceType?
     
-    override init(linkId: LinkIdType, shape: GeoPolylineType?, firstPoint: Int?, lastPoint: Int?, length: DistanceType?, remainDistance: DistanceType?, remainTime: DurationType?, nextLink: LinkIdType?, maneuver: ElementReferenceType?, timeDependentRestrictionRef: Int?, nextStopName: String?, line: ElementReferenceType?) {
+    init(linkId: LinkIdType, shape: GeoPolylineType?, firstPoint: Int?, lastPoint: Int?, length: DistanceType?, remainDistance: DistanceType?, remainTime: DurationType?, nextLink: LinkIdType?, maneuver: ElementReferenceType?, timeDependentRestrictionRef: Int?, nextStopName: String?, line: ElementReferenceType?) {
         super.init(linkId: linkId, shape: shape, firstPoint: firstPoint, lastPoint: lastPoint, length: length, remainDistance: remainDistance, remainTime: remainTime, nextLink: nextLink, maneuver: maneuver, timeDependentRestrictionRef: timeDependentRestrictionRef)
         
         self.nextStopName = nextStopName
