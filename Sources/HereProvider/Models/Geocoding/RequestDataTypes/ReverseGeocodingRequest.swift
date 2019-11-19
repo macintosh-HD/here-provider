919af949651fe638a68b66688a93d4a8443f78ab
Julian Gentges <JulGentges@gmail.com>
2019-11-19 21:47:38 +0100

    fix typos

--- Sources/HereProvider/Models/Geocoding/RequestDataTypes/ReverseGeocodingRequest.swift
+++ Sources/HereProvider/Models/Geocoding/RequestDataTypes/ReverseGeocodingRequest.swift
@@ -1,9 +1,9 @@
 public class ReverseGeocodingRequest: MultiReverseGeocodeRequest {
     let pos: Coordinate
     
-    init(additionalData: [KeyValuePairType<String, String>], addressAttributes: HereAddressAttributes, appID: String, appCode: String, gen: Int, jsonAttributes: Int, jsonCallback: String, language: LanguageCodeType, locationAttributes: HereLocationAttributes, maxResults: Int, pageInformation: String, politicalView: String, prox: GeoProximityType, responseAttributes: HereResponseAttributes, strictLanguage: Bool, level: String, minResults: Int, mode: HereMode, sortBy: String, pos: Coordinate) {
+    init(additionalData: [KeyValuePairType<String, String>], addressAttributes: HereAddressAttributes, appID: String, appCode: String, gen: Int, jsonAttributes: Int, jsonCallback: String, language: LanguageCodeType, locationAttributes: HereLocationAttributes, maxResults: Int, pageInformation: String, politicalView: String, prox: GeoProximityType, responseAttributes: HereResponseAttributes, strictLanguageMode: Bool, level: String, minResults: Int, mode: HereMode, sortBy: String, pos: Coordinate) {
         self.pos = pos
 
-        super.init(additionalData: additionalData, addressAttributes: addressAttributes, appID: appID, appCode: appCode, gen: gen, jsonAttributes: jsonAttributes, jsonCallback: jsonCallback, language: langugage, locationAttributes: locationAttributes, maxResults: maxResults, pageInformation: pageInformation, politicalView: politicalView, prox: prox, responseAttributes: responseAttributes, strictLanguageMode: strictLanguageMode, level: level, minResults: minResults, mode: mode, sortBy: sortBy)        
+        super.init(additionalData: additionalData, addressAttributes: addressAttributes, appID: appID, appCode: appCode, gen: gen, jsonAttributes: jsonAttributes, jsonCallback: jsonCallback, language: language, locationAttributes: locationAttributes, maxResults: maxResults, pageInformation: pageInformation, politicalView: politicalView, prox: prox, responseAttributes: responseAttributes, strictLanguageMode: strictLanguageMode, level: level, minResults: minResults, mode: mode, sortBy: sortBy)        
     }
 }
