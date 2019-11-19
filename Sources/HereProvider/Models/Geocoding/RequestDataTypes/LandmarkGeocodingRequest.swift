a59891a07638b9fb34db151ec0405653fe037c8f
Julian Gentges <JulGentges@gmail.com>
2019-11-19 21:32:27 +0100

    fixed super calls

--- Sources/HereProvider/Models/Geocoding/RequestDataTypes/LandmarkGeocodingRequest.swift
+++ Sources/HereProvider/Models/Geocoding/RequestDataTypes/LandmarkGeocodingRequest.swift
@@ -3,9 +3,9 @@ public class LandmarkGeocodeRequest: GeocodingRequest {
     let name: String
     
     init(additionalData: [KeyValuePairType<String, String>], addressAttributes: HereAddressAttributes, appID: String, appCode: String, gen: Int, jsonAttributes: Int, jsonCallback: String, language: LanguageCodeType, locationAttributes: HereLocationAttributes, maxResults: Int, pageInformation: String, politicalView: String, prox: GeoProximityType, responseAttributes: HereResponseAttributes, strictLanguage: Bool, bbox: GeoBoundingBoxType, city: String, country: String, countryFocus: String, county: String, district: String, houseNumber: String, locationId: String, mapView: GeoBoundingBoxType, postalCode: String, searchText: String, state: String, street: String, categoryIds: Int, name: String) {
-        super.init(additionalData: additionalData, addressAttributes: addressAttributes, appID: appID, appCode: appCode, gen: gen, jsonAttributes: jsonAttributes, jsonCallback: jsonCallback, language: language, locationAttributes: locationAttributes, maxResults: maxResults, pageInformation: pageInformation, politicalView: politicalView, prox: prox, responseAttributes: responseAttributes, strictLanguage: strictLanguage, bbox: bbox, city: city, country: country, countryFocus: countryFocus, county: county, district: district, houseNumber: houseNumber, locationId: locationId, mapView: mapView, postalCode: postalCode, searchText: searchText, state: state, street: street)
-        
         self.categoryIds = categoryIds
         self.name = name
+
+        super.init(additionalData: additionalData, addressAttributes: addressAttributes, appID: appID, appCode: appCode, gen: gen, jsonAttributes: jsonAttributes, jsonCallback: jsonCallback, language: language, locationAttributes: locationAttributes, maxResults: maxResults, pageInformation: pageInformation, politicalView: politicalView, prox: prox, responseAttributes: responseAttributes, strictLanguage: strictLanguage, bbox: bbox, city: city, country: country, countryFocus: countryFocus, county: county, district: district, houseNumber: houseNumber, locationId: locationId, mapView: mapView, postalCode: postalCode, searchText: searchText, state: state, street: street)        
     }
 }
