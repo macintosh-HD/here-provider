c881b68b7dd80f435afe1714471abfe54c2846a0
Julian Gentges <JulGentges@gmail.com>
2019-11-19 20:50:12 +0100

    used protocol approach for geocode requests

--- Sources/HereProvider/Models/Geocoding/RequestDataTypes/GeocodingRequest.swift
+++ Sources/HereProvider/Models/Geocoding/RequestDataTypes/GeocodingRequest.swift
@@ -1,4 +1,4 @@
-public struct GeocodingRequest: HereGeocodeRequest {
+public class GeocodingRequest: HereGeocodeRequest {
     // inherited properties
     let additionalData: [KeyValuePairType<String, String>]
     let addressAttributes: HereAddressAttributes
@@ -29,4 +29,16 @@ public struct GeocodingRequest: HereGeocodeRequest {
     let searchText: String
     let state: String
     let street: String
+    
+    init(additionalData: [KeyValuePairType<String, String>], addressAttributes: HereAddressAttributes, appID: String, appCode: String, gen: Int, jsonAttributes: Int, jsonCallback: String, language: LanguageCodeType, locationAttributes: HereLocationAttributes, maxResults: Int, pageInformation: String, politicalView: String, prox: GeoProximityType, responseAttributes: HereResponseAttributes, strictLanguage: Bool, bbox: GeoBoundingBoxType, city: String, country: String, countryFocus: String, county: String, district: String, houseNumber: String, locationId: String, mapView: GeoBoundingBoxType, postalCode: String, searchText: String, state: String, street: String) {
+        self.additionalData = additionalData
+        self.addressAttributes = addressAttributes
+        self.appID = appID
+        self.appCode = appCode
+        self.gen = gen
+        self.jsonAttributes = jsonAttributes
+        self.jsonCallback = jsonCallback
+        self.langugage = language
+        self.locationAttributes = locationAttributes
+    }
 }
--- Sources/HereProvider/Models/Geocoding/RequestDataTypes/MultiReverseGeocodingRequest.swift
+++ Sources/HereProvider/Models/Geocoding/RequestDataTypes/MultiReverseGeocodingRequest.swift
@@ -1,4 +1,4 @@
-public struct MultiReverseGeocodeRequest: HereGeocodeRequest {
+public class MultiReverseGeocodeRequest: HereGeocodeRequest {
     // inherited properties
     let additionalData: [KeyValuePairType<String, String>]
     let addressAttributes: HereAddressAttributes
@@ -20,4 +20,27 @@ public struct MultiReverseGeocodeRequest: HereGeocodeRequest {
     let minResults: Int
     let mode: HereMode
     let sortBy: String
+    
+    init(additionalData: [KeyValuePairType<String, String>], addressAttributes: HereAddressAttributes, appID: String, appCode: String, gen: Int, jsonAttributes: Int, jsonCallback: String, language: LanguageCodeType, locationAttributes: HereLocationAttributes, maxResults: Int, pageInformation: String, politicalView: String, prox: GeoProximityType, responseAttributes: HereResponseAttributes, strictLanguage: Bool, level: String, minResults: Int, mode: HereMode, sortBy: String) {
+        self.additionalData = additionalData
+        self.addressAttributes = addressAttributes
+        self.appID = appID
+        self.appCode = appCode
+        self.gen = gen
+        self.jsonAttributes = jsonAttributes
+        self.jsonCallback = jsonCallback
+        self.langugage = language
+        self.locationAttributes = locationAttributes
+        self.maxResults = maxResults
+        self.pageInformation = pageInformation
+        self.politicalView = politicalView
+        self.prox = prox
+        self.responseAttributes = responseAttributes
+        self.strictLanguageMode = strictLanguageMode
+        
+        self.level = level
+        self.minResults = minResults
+        self.mode = mode
+        self.sortBy = sortBy
+    }
 }
