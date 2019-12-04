8d3f8841cd64473bee2c2a44f66eafd10d607426
Julian Gentges <JulGentges@gmail.com>
2019-12-05 00:08:32 +0100

    conform `HereCoordinate` to `Content` to be useable for Fluent models

--- Sources/HereProvider/Models/Geocoding/ResponseDataTypes/BaseDataTypes/Types.swift
+++ Sources/HereProvider/Models/Geocoding/ResponseDataTypes/BaseDataTypes/Types.swift
@@ -210,7 +210,7 @@ public enum HereFormats: String {
     case json, xml
 }
 
-public struct HereCoordinate {
+public struct HereCoordinate: Content {
     public let lon: Double
     public let lat: Double
     public let bearing: Int
